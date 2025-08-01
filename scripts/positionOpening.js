63cbe7c1cd1d2583a363e78414675b32a6eb75ff
chloeluk852 <78545300+chloeluk852@users.noreply.github.com>
2025-08-01 14:57:10 +0800

    Update positionOpening.js
    phone-friendly

--- scripts/positionOpening.js
+++ scripts/positionOpening.js
@@ -21,32 +21,17 @@ class OpeningPosition {
     ctx.fillText("Residence Massacre", play.width / 2, play.height / 2 - 70);
 
     // Draw start game instruction
-    ctx.font = "40px Inria Serif";
-    ctx.fillStyle = "#EDF5E1";
-    ctx.fillText(
-      "Hi Noob, let's defeat Namu!",
-      play.width / 2,
-      play.height / 2
-    );
-    ctx.fillText(
-      "Press 'Space' to start.",
-      play.width / 2,
-      play.height / 2 + 100
-    );
+    ctx.font = "40px Serif";
+    ctx.fillStyle = '#D7DF01';
+    ctx.fillText("Press 'Space' or tap 'FIRE' to start.", play.width / 2, play.height / 2);
 
     // Draw game controls instruction
-    ctx.fillStyle = "#05386B";
-    ctx.fillText("Game Controls", play.width / 2, play.height / 2 + 210);
-    ctx.fillText(
-      "Left Arrow : Move Left",
-      play.width / 2,
-      play.height / 2 + 260
-    );
-    ctx.fillText(
-      "Right Arrow : Move Right",
-      play.width / 2,
-      play.height / 2 + 300
-    );
+    ctx.fillStyle = '#2e2f00';
+    ctx.fillText("Game Controls", play.width / 2, play.height / 2 + 180);
+    ctx.font = "30px Serif";
+    ctx.fillText("Use on-screen buttons to move and fire on mobile", play.width / 2, play.height / 2 + 220);
+    ctx.fillText("Left Arrow : Move Left", play.width / 2, play.height / 2 + 260);
+    ctx.fillText("Right Arrow : Move Right", play.width / 2, play.height / 2 + 300);
     ctx.fillText("Space : Fire", play.width / 2, play.height / 2 + 340);
   }
 
