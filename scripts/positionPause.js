a24f3600857c15bbd4138e6289b1b9959ff58934
chloeluk852 <78545300+chloeluk852@users.noreply.github.com>
2025-08-01 14:59:16 +0800

    Update positionPause.js
    phone-friendly

--- scripts/positionPause.js
+++ scripts/positionPause.js
@@ -4,43 +4,15 @@ class PausePosition {
   draw(play) {
     ctx.clearRect(0, 0, play.width, play.height);
     ctx.font = "40px Inria Serif";
-    ctx.fillStyle = "#ffffff";
-    ctx.textAlign = "center";
-    ctx.fillText(
-      "YOUR FIGHTING MEANS NOTHING -Larry",
-      play.width / 2,
-      play.height / 2 - 300
-    );
-
-    ctx.fillStyle = "#5CDB95";
-    ctx.font = "36px Inria Serif";
-    ctx.fillText(
-      "P: resume total annhilation of larry",
-      play.width / 2,
-      play.height / 2 - 250
-    );
-    ctx.fillText("ESC: Quit Game", play.width / 2, play.height / 2 - 210);
-
-    ctx.font = "40px Inria Serif";
-    ctx.fillStyle = "#ffffff";
-    ctx.fillText(
-      "Game controls reminder",
-      play.width / 2,
-      play.height / 2 - 120
-    );
-    ctx.fillStyle = "#5CDB95";
-    ctx.font = "36px Inria Serif";
-    ctx.fillText(
-      "Left Arrow : Move Left",
-      play.width / 2,
-      play.height / 2 - 70
-    );
-    ctx.fillText(
-      "Right Arrow : Move Right",
-      play.width / 2,
-      play.height / 2 - 30
-    );
-    ctx.fillText("Space : Fire", play.width / 2, play.height / 2 + 10);
+    ctx.fillStyle = '#ffffff';
+    ctx.fillText("Game controls reminder", play.width / 2, play.height/2 - 120);
+    ctx.fillStyle = '#D7DF01';
+    ctx.font="30px Inria Serif";
+    ctx.fillText("On Mobile: Use on-screen buttons", play.width / 2, play.height/2 - 80);
+    ctx.font="36px Inria Serif";
+    ctx.fillText("Left Arrow : Move Left", play.width / 2, play.height/2 - 30);
+    ctx.fillText("Right Arrow : Move Right", play.width / 2, play.height/2 + 10);
+    ctx.fillText("Space : Fire", play.width / 2, play.height/2 + 50);
   }
   keyDown(play, keyboardCode) {
     // keyDown method code goes here
